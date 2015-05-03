d25bfb61fb6b1db4a59bac61e6c3402882742e3a
Navin Pai <lifeofnavin@gmail.com>
2015-05-03 23:41:46 +0530

    Initial commit

---
+++ content/hello-world.md
@@ -0,0 +1,24 @@
+Title: Hello World
+Date: 2015-05-03 10:23
+Modified: 2015-05-03 10:23
+Category: gsoc
+Tags: gsoc, dbpedia
+Slug: hello-world
+Authors: Navin Pai
+Summary: Introductory post
+
+Hey there!
+
+I'm Navin, and this is where I will be posting updates about the stuff I learn/explore as part of GSoC 2015. 
+
+
+I'm going to be working with the fine people at the DBpedia project *(to be more specific, the JSONpedia project)* and will be working on enhancing the extractors used for extracting information from Wikipedia.
+
+ 
+For a quick overview of my project, visit [my project proposal page][1].
+
+
+Other quick link are in the left menu.
+
+
+[1]: https://www.google-melange.com/gsoc/project/details/google/gsoc2015/lifeofnavin/5676830073815040
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Navin Pai'
+SITENAME = u'The GSoC Diaries'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+THEME='themes/svbhack'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = [('GSoC','https://www.google-melange.com/gsoc/homepage/google/gsoc2015'),('DBpedia','http://wiki.dbpedia.org'), ('JSONpedia','http://jsonpedia.org')]
+
+# Social widget
+SOCIAL = (('@navinpai', 'http://twitter.com/navinpai'),
+		('Personal Blog', 'http://lifeofnav.in'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+###### SPECIFIC TO SVBHACK
+
+USER_LOGO_URL = SITEURL + '/theme/images/logo.jpg'
+TAGLINE = 'GSoC 2015 | DBpedia'
+#######
+
+OUTPUT_PATH = 'outpt/'
