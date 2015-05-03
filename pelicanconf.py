bbf5e4813c4429c405d6fe8c8945a699eab6bb27
Navin Pai <lifeofnavin@gmail.com>
2015-05-03 23:50:09 +0530

    Fixed config

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ SOCIAL = (('@navinpai', 'http://twitter.com/navinpai'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 
 ###### SPECIFIC TO SVBHACK
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://navinpai.github.io'
+SITEURL = 'http://navinpai.github.io/gsoc-blog/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
